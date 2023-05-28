5b606e387f0cb510f1613f9045feb88013822aba
Abhinav Garg <abhinavgarg2004@gmail.com>
2023-05-28 12:08:16 +0530

    added news cards and structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react'
-
-import React, { Component } from 'react'
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import NewsItem from "./components/NewsItem";
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
-    )
+      <>
+        <Navbar />
+        <News/>
+
+      </>
+    );
   }
 }

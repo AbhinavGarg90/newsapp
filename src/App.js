fa448b5a9468c7d1386ad210f4d2d6b225dfe0d7
Abhinav Garg <abhinavgarg2004@gmail.com>
2023-05-31 09:07:29 +0530

    31 may

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
-import NewsItem from "./components/NewsItem";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
-        <News/>
 
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="business" element={<News key="business" pageSize={20} country="in" category="business" />}></Route>
+
+            <Route path="entertainment" element={<News key="entertainment" pageSize={20} country="in" category="entertainment" />}></Route>
+
+            <Route path="/" element={<News key="home" pageSize={20} country="in" category="general" />}></Route>
+
+            <Route path="health" element={<News key="health" pageSize={20} country="in" category="health" />}></Route>
+
+            <Route path="science" element={<News key="scienec" pageSize={20} country="in" category="science" />}></Route>
+
+          </Routes>
+        </Router>
       </>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 
-export default class extends Component {
+export default class NewsItem extends Component {
 
     render() {
         let { title, description, imageUrl, newsUrl } = this.props
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card" >
+                    <img src={imageUrl ? imageUrl: "https://scx2.b-cdn.net/gfx/news/2023/a-worm-inspired-robot.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} className="btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
             </div>
